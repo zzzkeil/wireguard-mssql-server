b303145d5125bbbef94147d466293b4aab7cae6d
Klaus <38099918+zzzkeil@users.noreply.github.com>
2021-07-08 18:09:00 +0200

    Update test_x86.sh

--- ubuntu20.04/test_x86.sh
+++ ubuntu20.04/test_x86.sh
@@ -176,11 +176,11 @@ systemctl stop mssql-server
 /opt/mssql/bin/mssql-conf set network.tcpport $dbport
 
 
-openssl req -x509 -nodes -newkey rsa:4096 -subj '/$(hostname -I | awk '{print $1}')' -keyout mssql.key -out mssql.pem -days 365 
+openssl req -x509 -nodes -newkey rsa:4096 -keyout mssql.key -out mssql.pem -days 365 
 chown mssql:mssql mssql.pem mssql.key 
 chmod 600 mssql.pem mssql.key 
 # in this case we are saving the certificate to the certs folder under /etc/ssl/ which has the following permission 755(drwxr-xr-x)
-mv mssql.pem /etc/ssl/certs/ drwxr-xr-x 
+mv mssql.pem /etc/ssl/certs/
 # in this case we are saving the private key to the private folder under /etc/ssl/ with permissions set to 755(drwxr-xr-x)
 mv mssql.key /etc/ssl/private/ 
 
@@ -188,7 +188,7 @@ mv mssql.key /etc/ssl/private/
 cat /var/opt/mssql/mssql.conf 
 /opt/mssql/bin/mssql-conf set network.tlscert /etc/ssl/certs/mssql.pem 
 /opt/mssql/bin/mssql-conf set network.tlskey /etc/ssl/private/mssql.key 
-/opt/mssql/bin/mssql-conf set network.tlsprotocols 1.2 
+/opt/mssql/bin/mssql-conf set network.tlsprotocols 1.2
 /opt/mssql/bin/mssql-conf set network.forceencryption 1
 systemctl restart mssql-server 
 
